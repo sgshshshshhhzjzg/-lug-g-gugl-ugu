e035fbabb792d0e4d44acf79d5cbb5fe379f6ea9
Glitch (caramel-magenta-trollius) <none>
2021-02-17 14:12:31 +0000

    🏐🎨 Checkpoint ./komutlar/mute-log.js:21496408/3892 ./komutlar/sohbet-aç.js:28701561/1087

--- komutlar/mute-log.js
+++ komutlar/mute-log.js
@@ -1,6 +1,46 @@
-const Discord = require('discord.js')
-const db = require('quick.db')
+const Discord = require("discord.js");
+const db = require("quick.db");
+const ayarlar = require('../ayarlar.json')
 
 exports.run = async (client, message, args) => {
+  if (!message.member.permission.has("MANAGE_ROLES"))
+    return message.channel.send(
+      new Discord.MessageEmbed()
+        .setDescription(
+          "Bu Komutu Kullanabilmek İçin `Rolleri Yönet` Yetkisine Sahip Olmalısın !"
+        )
+        .setColor("RANDOM")
+    );
   
-}
+  let logkanal = message.mentions.channels.first()
+  
+  if(!logkanal) return message.channel.send(new Discord.MessageEmbed().setDescription("Mute Lo").setColor("RANDOM"))
+  
+  if(args[0] === "ayarla") {
+    db.set(`mutelog_${message.guild.id}`, logkanal.id)
+    const embed = new Discord.MessageEmbed()
+    .setDescription(`Mute Log Kanalı Başarıyla ${logkanal} Olarak Ayarlandı !`)
+    .setColor(ayarlar.oldu)
+    message.channel.send(embed)
+  }
+  
+  if(args[0] === "sıfırla") {
+    db.delete(`mutelog_${message.guild.id}`)
+    const embed1 = new Discord.MessageEmbed()
+    .setDescription("Mute Log Kanalı Başarıyla Sıfırlandı !")
+    .setColor(ayarlar.oldu)
+    message.channel.send(embed1)
+  }
+  
+};
+
+exports.conf = {
+  enabled: true,
+  guildOnly: false,
+  aliases: [""],
+  permlvl: 1 //Ccd Code
+};
+
+exports.help = {
+  name: "mute-log"
+}; //Ccd Code

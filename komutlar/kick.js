edfcb13b45d7daaae21865c17d8e36caf6c05552
Glitch (caramel-magenta-trollius) <none>
2021-02-17 13:03:57 +0000

    💧🙆 Checkpoint ./komutlar/kick.js:28701561/1312

---
+++ komutlar/kick.js
@@ -0,0 +1,45 @@
+const Discord = require("discord.js");
+const db = require("quick.db");
+const ayarlar = "../ayarlar.json"; //Ccd Code
+
+exports.run = async (client, message, args) => {
+  if (!message.member.permissions.has("KICK_MEMBERS")) //Ccd Code
+    return message.channel.send(
+      new Discord.MessageEmbed()
+        .setDescription(
+          "Bu Komutu Kullanabilmek İçin `Üyeleri Banla` Yetkisine Sahip Olmalısın !" //Ccd Code
+        )
+        .setColor("RANDOM")
+    );
+
+  let kullanıcı = message.mentions.members.first();
+  let oldu = ayarlar.oldu;
+  let log = db.fetch(`banlog_${message.guild.id}`);
+
+  message.guild.member(kullanıcı).kick();
+  
+  const başarılı = new Discord.MessageEmbed() //Ccd Code
+    .setTitle("Kullanıcı Banlandı !")
+    .addField("Banlanan Kullanıcı", kullanıcı) //Ccd Code
+    .addField("Banlayan Yetkili", message.author)
+    .setColor(oldu);
+  message.channel.send(başarılı);
+//Ccd Code
+  const logmesaj = new Discord.MessageEmbed()
+    .setTitle("Ban Log !")
+    .addField("Banlanan Kullanıcı", kullanıcı)
+    .addField("Banlayan Yetkili", message.author) //Ccd Code
+    .setColor(oldu);
+  client.channels.cache.get(log).send(logmesaj);
+};
+
+exports.conf = {
+  enabled: true,
+  guildOnly: false,
+  aliases: [""],
+  permlvl: 1
+};
+
+exports.help = {
+  name: "ban"
+}; //Ccd Code

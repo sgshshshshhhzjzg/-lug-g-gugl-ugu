e7b4a87bc417603fd589492ca0a9e089b1f61b0d
Glitch (caramel-magenta-trollius) <none>
2021-02-17 12:05:04 +0000

    🎥🍴 Checkpoint ./komutlar/ban.js:21496408/4356 ./komutlar/kick.js:28701561/772 ./komutlar/ban-log.js:21496408/1327

--- komutlar/ban-log.js
+++ komutlar/ban-log.js
@@ -1,9 +1,18 @@
-const Discord = require('discord.js')
-const db = require('quick.db')
+const Discord = require("discord.js");
+const db = require("quick.db");
 
 exports.run = async (client, message, args) => {
+  if (!message.member.hasPermissions("BAN_MEMBERS"))
+    return message.channel.send(
+      new Discord.MessageEmbed()
+        .setDescription(
+          "Bu Komutu Kullanabilmek İçin `Üyeleri Banla` Yetkisine Sahip Olmalısın !"
+        )
+        .setColor("RANDOM")
+    );
   
-}
+  
+};
 
 exports.conf = {
   enabled: true,
--- komutlar/ban.js
+++ komutlar/ban.js
@@ -1,9 +1,35 @@
-const Discord = require('discord.js')
-const db = require('quick.db')
+const Discord = require("discord.js");
+const db = require("quick.db");
+const ayarlar = "../ayarlar.json";
 
 exports.run = async (client, message, args) => {
-  
-}
+  if (!message.member.hasPermissions("BAN_MEMBERS"))
+    return message.channel.send(
+      new Discord.MessageEmbed()
+        .setDescription(
+          "Bu Komutu Kullanabilmek İçin `Üyeleri Banla` Yetkisine Sahip Olmalısın !"
+        )
+        .setColor("RANDOM")
+    );
+
+  let kullanıcı = message.mentions.members.first();
+  let oldu = ayarlar.oldu;
+  let log = db.fetch(`banlog_${message.guild.id}`);
+
+  const başarılı = new Discord.MessageEmbed()
+    .setTitle("Kullanıcı Banlandı !")
+    .addField("Banlanan Kullanıcı", kullanıcı)
+    .addField("Banlayan Yetkili", message.author)
+    .setColor(oldu);
+  message.channel.send(başarılı);
+
+  const logmesaj = new Discord.MessageEmbed()
+    .setTitle("Ban Log !")
+    .addField("Banlanan Kullanıcı", kullanıcı)
+    .addField("Banlayan Yetkili", message.author)
+    .setColor(oldu);
+  message.channel.send(log);
+};
 
 exports.conf = {
   enabled: true,
